f9055bf66faa9732147a18e017ffab1671560790
Mykola Kovalskyi <112631611+mykolakovalskyi@users.noreply.github.com>
2023-07-13 17:05:37 +0200

    localStorage update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,33 +1,9 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getContacts } from 'redux/selectors';
-import { setContacts } from 'redux/contactsSlice';
 import ContactForm from '../ContactForm/ContactForm';
 import Filter from '../Filter/Filter';
 import ContactList from '../ContactList/ContactList';
 import css from './App.module.css';
 
 export default function App() {
-  const contacts = useSelector(getContacts);
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    try {
-      const savedContacts = localStorage.getItem('contacts');
-      const parsedContacts = JSON.parse(savedContacts);
-      if (parsedContacts) {
-        dispatch(setContacts(parsedContacts));
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }, [dispatch]);
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
   return (
     <div className={css.wraper}>
       <h1>Phonebook</h1>
